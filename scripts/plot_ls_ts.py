14c9e6681b7f7c2dab43c2fbd68251924c9c0bb9
Hai Pham <pvh1983@gmail.com>
2020-02-24 10:37:59 -0800

    Before updating 2019 gwlevel data

--- scripts/plot_ls_ts.py
+++ scripts/plot_ls_ts.py
@@ -32,17 +32,27 @@ sat = 'sen'   # 'alos' or 'sen'
 cur_dir = os.getcwd()  # get the current working directory
 sampling_freq = 'M'    # Note: Check freq of gwlevel at line 48
 start_date = pd.Timestamp(dt.date(2005, 1, 1))
-end_date = pd.Timestamp(dt.date(2019, 12, 31))
+end_date = pd.Timestamp(dt.date(2020, 2, 28))
 df_date = gen_date_time_series(start_date, end_date, sampling_freq)
 
 
 # [1] LOAD DATA ===============================================================
 # [1.1] Specify an input file that has the time series of subsidence velocity
 # at groundwater level observation wells
-###ifile_ls = r'../input/ts_ls_all_points.csv'
-#ifile_ls = r'../input/ts_ls_at_well_logs/ts_ls_at_well_logs_sen.csv'
-ifile_ls = r'../input/ts_ls_at_well_logs/ts_ls_at_well_logs_' + \
-    sat + '_' + domain + '.csv'
+# ifile_ls = r'../input/ts_ls_at_well_logs/ts_ls_at_well_logs_' + \
+#    sat + '_' + domain + '.csv'
+
+
+# [1] calculate subsidence at given points from 06/14/2015 to 03/25/2019
+ifile_ts_ls_alos = 'c:/Users/hpham/Documents/P31 Maki/ML4sub/input/ts_ls_at_well_logs/v02_02202020/ts_ls_at_well_logs_alos_pv.csv'
+print(f'Reading file {ifile_ts_ls_alos}\n')
+#dfls_alos = pd.read_csv(ifile_ts_ls_alos)
+#dfls_alos['Date'] = pd.to_datetime(dfls_alos['Date'])
+
+ifile_ts_ls_sen = 'c:/Users/hpham/Documents/P31 Maki/ML4sub/input/ts_ls_at_well_logs/v02_02202020/ts_ls_at_well_logs_sen_asc_run4.csv'
+print(f'Reading file {ifile_ts_ls_sen}\n')
+#dfls_sen = pd.read_csv(ifile_ts_ls_sen)
+#dfls_sen['Date'] = pd.to_datetime(dfls_sen['Date'])
 
 
 # [1.2] Specify the path to time series groundwater level data
@@ -50,17 +60,24 @@ ifile_ls = r'../input/ts_ls_at_well_logs/ts_ls_at_well_logs_' + \
 ifile_gwlevel = r'../input/gwlevels__freq_M.csv'
 
 # [1.3] Load data and resampling
-dfs, dfgw = read_input(ifile_ls, ifile_gwlevel, sampling_freq, start_date)
+dfls_alos, dfgw = read_input(ifile_ts_ls_alos, ifile_gwlevel,
+                             sampling_freq, start_date)
+dfls_sen, dfgw = read_input(ifile_ts_ls_sen, ifile_gwlevel,
+                            sampling_freq, start_date)
+
+print(dfls_alos.shape)
+print(dfls_sen.shape)
+
 
 # [] Create a new folder to save the figures
-odir = '../output/head_vs_ls_' + sat + '_' + domain + '/'
+odir = '../output/head_vs_ls_' + sat + '_' + domain + '_' + sampling_freq + '/'
 if not os.path.exists(odir):  # Make a new directory if not exist
     os.makedirs(odir)
     print(f'\nCreated directory {odir}\n')
 
 # [] Plot ls vs.hed
 if opt_plot_ls_vs_head:
-    plot_ls_vs_hed(dfs, dfgw, start_date, end_date, odir)
+    plot_ls_vs_hed(dfls_alos, dfls_sen, dfgw, start_date, end_date, odir)
 
 # [] Calculate trends
 if opt_cal_trend:
